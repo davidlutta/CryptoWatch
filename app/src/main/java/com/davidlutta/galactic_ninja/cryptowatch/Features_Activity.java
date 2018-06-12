9ef82239afcc07e8f00f5c91451a9b65571b524f
davidlutta <davidlutta2010@gmail.com>
2018-06-12 11:28:12 +0300

    Add array of features and toasts

--- app/src/main/java/com/davidlutta/galactic_ninja/cryptowatch/Features_Activity.java
+++ app/src/main/java/com/davidlutta/galactic_ninja/cryptowatch/Features_Activity.java
@@ -1,10 +1,14 @@
 package com.davidlutta.galactic_ninja.cryptowatch;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -14,6 +18,7 @@ public class Features_Activity extends AppCompatActivity {
     ListView mlistView;
     @Bind(R.id.featuresTextView)
     TextView mFeaturesTextView;
+    private String[] features = {"find the latest prices of CryptoCurrencies","find out the market capital of CryptoCurrencies"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,14 @@ public class Features_Activity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        mlistView.setAdapter(adapter);
+        mlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String feature = ((TextView)view).getText().toString();
+                Toast.makeText(Features_Activity.this, feature,Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
