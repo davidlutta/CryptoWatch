566931c64c7760a959cefd9ef66d9393e97de41a
davidlutta <davidlutta2010@gmail.com>
2018-06-12 11:29:19 +0300

    Set typeface on text view

--- app/src/main/java/com/davidlutta/galactic_ninja/cryptowatch/Features_Activity.java
+++ app/src/main/java/com/davidlutta/galactic_ninja/cryptowatch/Features_Activity.java
@@ -26,7 +26,8 @@ public class Features_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_features_);
 
         ButterKnife.bind(this);
-
+        Typeface cool = Typeface.createFromAsset(getAssets(),"fonts/coolvetica.ttf");
+        mFeaturesTextView.setTypeface(cool);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
         mlistView.setAdapter(adapter);
         mlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
