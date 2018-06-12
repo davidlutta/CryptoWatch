9e253aab00da40ff59db5d93bda1c37a067ae5d2
davidlutta <davidlutta2010@gmail.com>
2018-06-12 12:57:11 +0300

    Change fonts of search Activity

--- app/src/main/java/com/davidlutta/galactic_ninja/cryptowatch/Search_Activity.java
+++ app/src/main/java/com/davidlutta/galactic_ninja/cryptowatch/Search_Activity.java
@@ -1,12 +1,27 @@
 package com.davidlutta.galactic_ninja.cryptowatch;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.Bind;
 
 public class Search_Activity extends AppCompatActivity {
+    @Bind(R.id.searchTextView) TextView mSearchTextView;
+    @Bind(R.id.searchEditText) EditText mSearchEditText;
+    @Bind(R.id.searchButton) Button mSearchButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_);
+
+        Typeface cool = Typeface.createFromAsset(getAssets(),"fonts/coolvetica.ttf");
+        mSearchButton.setTypeface(cool);
+        mSearchEditText.setTypeface(cool);
+        mSearchTextView.setTypeface(cool);
     }
 }
