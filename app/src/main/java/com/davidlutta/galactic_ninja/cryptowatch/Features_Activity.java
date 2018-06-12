602fe02614c6da0c7f42dbdf838970fe05d7f681
davidlutta <davidlutta2010@gmail.com>
2018-06-12 12:20:59 +0300

    Add new Button and change styling

--- app/src/main/java/com/davidlutta/galactic_ninja/cryptowatch/Features_Activity.java
+++ app/src/main/java/com/davidlutta/galactic_ninja/cryptowatch/Features_Activity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,10 +15,9 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class Features_Activity extends AppCompatActivity {
-    @Bind(R.id.listView)
-    ListView mlistView;
-    @Bind(R.id.featuresTextView)
-    TextView mFeaturesTextView;
+    @Bind(R.id.listView) ListView mlistView;
+    @Bind(R.id.featuresTextView) TextView mFeaturesTextView;
+    @Bind(R.id.nextButton) Button mNextButton;
     private String[] features = {"find the latest prices of CryptoCurrencies","find out the market capital of CryptoCurrencies"};
 
     @Override
@@ -28,6 +28,8 @@ public class Features_Activity extends AppCompatActivity {
         ButterKnife.bind(this);
         Typeface cool = Typeface.createFromAsset(getAssets(),"fonts/coolvetica.ttf");
         mFeaturesTextView.setTypeface(cool);
+        mNextButton.setTypeface(cool);
+
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
         mlistView.setAdapter(adapter);
         mlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
