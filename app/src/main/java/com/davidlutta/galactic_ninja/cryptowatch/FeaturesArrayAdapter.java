914d2df21185c28d2a34d53cb857a71e73318092
davidlutta <davidlutta2010@gmail.com>
2018-06-13 11:13:49 +0300

    Edit FeaturesArrayAdapter

--- app/src/main/java/com/davidlutta/galactic_ninja/cryptowatch/FeaturesArrayAdapter.java
+++ app/src/main/java/com/davidlutta/galactic_ninja/cryptowatch/FeaturesArrayAdapter.java
@@ -5,4 +5,11 @@ import android.widget.ArrayAdapter;
 
 public class FeaturesArrayAdapter extends ArrayAdapter {
     private Context mContext;
+    private String[] mFeatures;
+
+    public FeaturesArrayAdapter(Context mContext,int resource, String[] mFeatures){
+        super(mContext,resource);
+        this.mContext = mContext;
+        this.mFeatures = mFeatures;
+    }
 }
