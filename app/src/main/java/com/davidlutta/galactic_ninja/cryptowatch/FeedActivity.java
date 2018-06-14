665c2b83790b7e10ece564e1b86554b82f48edc9
davidlutta <davidlutta2010@gmail.com>
2018-06-14 16:22:17 +0300

    Add new Activity for Feeds

---
+++ app/src/main/java/com/davidlutta/galactic_ninja/cryptowatch/FeedActivity.java
@@ -0,0 +1,13 @@
+package com.davidlutta.galactic_ninja.cryptowatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
