e2026072661e5761f60e4dbedc5f617ebca0503c
davidlutta <davidlutta2010@gmail.com>
2018-06-20 12:40:21 +0300

    Add code to page Adapter

--- app/src/main/java/com/davidlutta/galactic_ninja/cryptowatch/ArrayAdapters/CurrencyPageAdapter.java
+++ app/src/main/java/com/davidlutta/galactic_ninja/cryptowatch/ArrayAdapters/CurrencyPageAdapter.java
@@ -1,4 +1,33 @@
 package com.davidlutta.galactic_ninja.cryptowatch.ArrayAdapters;
 
-public class CurrencyPageAdapter {
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.davidlutta.galactic_ninja.cryptowatch.UI.CurrencyDetailFragment;
+import com.davidlutta.galactic_ninja.cryptowatch.models.Results;
+
+import java.util.ArrayList;
+
+public class CurrencyPageAdapter extends FragmentPagerAdapter {
+    private ArrayList<Results> mResults;
+
+    public CurrencyDetailFragment(FragmentManager fm, ArrayList<Results> results){
+        super(fm);
+        mResults = results;
+    }
+
+
+    @Override
+    public Fragment getItem(int position){
+        return CurrencyDetailFragment.newInstace(mResults.get(position));
+    }
+    @Override
+    public int getCount() {
+        return mResults.size();
+    }
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mResults.get(position).getName();
+    }
 }
